a2aa33c4c735e8994541225f5437a763ca8880e0
SaKa1979 <sanderkamps79@gmail.com>
2014-09-09 19:09:44 +0200

    Added laatste kandidaat op laatste partij

--- LijstProgramma.java
+++ LijstProgramma.java
@@ -6,53 +6,27 @@ import verkiezingen.Kandidaat;
 public class LijstProgramma {
   public static void main(String[] args) {	        
 
+    int aantalPartijen;
+    int aantalKandidaten;
+    
 Stemmachine stemmachine = new Stemmachine(); //create an object of type Stemmachine
+stemmachine.zetAan(); //Creates partijen en kandidaten objects automatically
 
-//Elections 2006
-Partij cda = new Partij("CDA"); //create an object of type Partij
-Partij pvda = new Partij("PvdA");
-Partij vvd = new Partij("VVD");
-
-//Add party to stemmachine
-stemmachine.voegPartijToe(cda);
-stemmachine.voegPartijToe(pvda);
-stemmachine.voegPartijToe(vvd);
-
-//Create ArrayList of party's
+//Create ArrayList of partij
 ArrayList<Partij> partijlijst = stemmachine.getPartijen();
+//Get amount of partijen in partijlijst
+aantalPartijen=partijlijst.size();
+//Get laatste partij from partijlijst 
+Partij laatstepartij = partijlijst.get(aantalPartijen-1);
 
-//CDA
-Kandidaat janPeterBalkenende = new Kandidaat("Jan Peter Balkenende","Capelle aan den IJsel");
-Kandidaat maximeVerhagen = new Kandidaat("Maxime Verhagen","Voorburg");
-//PvdA
-Kandidaat wouterBos = new Kandidaat("Wouter Bos","Amsterdam");
-Kandidaat tonHeerts = new Kandidaat("Ton Heerts","Apeldoorn");
-//VVD
-Kandidaat markRutte = new Kandidaat("Mark Rutte","Den Haag");
-Kandidaat ritaVerdonk = new Kandidaat("Rita Verdonk","Nootdorp");
+//Create ArrayList of the candidates in the last (3d) partij
+ArrayList<Kandidaat> kandidatenlijst = laatstepartij.getKandidaten();
+//Get amount of kandidaten
+aantalKandidaten=kandidatenlijst.size();
+//Get laatste kandidaat from kandidatenlijst
+Kandidaat laatsteKandidaat = kandidatenlijst.get(aantalKandidaten-1);
 
-//Add candidate to party
-cda.voegKandidaatToe(janPeterBalkenende);
-cda.voegKandidaatToe(maximeVerhagen);
-pvda.voegKandidaatToe(wouterBos);
-pvda.voegKandidaatToe(tonHeerts);
-vvd.voegKandidaatToe(markRutte);
-vvd.voegKandidaatToe(ritaVerdonk);
-
-//Create ArrayList of candidates
-ArrayList<Kandidaat> kandidatenlijstCDA = cda.getKandidaten();
-ArrayList<Kandidaat> kandidatenlijstPvdA = pvda.getKandidaten();
-ArrayList<Kandidaat> kandidatenlijstVVD = vvd.getKandidaten();
-
-for(int i=0; i<3;i++){
-  Partij p = partijlijst.get(i);
-  System.out.println(p.getNaam());
-  for(int j=0;j<2;j++){
-    Kandidaat k = kandidatenlijstCDA.get(j);
-    System.out.println(k.getNaam());
-    }
-  }
-  
+System.out.println("De laatste kandidaat is "+ laatsteKandidaat.getNaam());
 
   }
 }
